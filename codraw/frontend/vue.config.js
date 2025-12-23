d20d5e4ae627d2f9dd7ea3b1385f7e1e7f896274
Damian0563 <damian.realmadrid2005@gmail.com>
2025-12-23 06:51:20 +0100

    prod adjustments

--- codraw/frontend/vue.config.js
+++ codraw/frontend/vue.config.js
@@ -5,4 +5,15 @@ module.exports = defineConfig({
     port: 8000,
     host: '0.0.0.0', // Necessary for Docker to bind correctly
   },
+  // devServer: {
+  //   port: 8000,
+  //   host: '0.0.0.0',
+  //   allowedHosts: ['codrawapp.com', 'www.codrawapp.com', '34.116.244.111'],
+  //   client: {
+  //     webSocketURL: 'wss://codrawapp.com/ws', 
+  //   },
+  //   headers: {
+  //     "Access-Control-Allow-Origin": "*",
+  //   }
+  // }
 })
